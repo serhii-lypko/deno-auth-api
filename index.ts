e07c5692a1b5ed4d6272dcb89daa7e0c5b7a202d
Serhii Lypko <lypko.sg@gmail.com>
2024-06-08 10:28:47 +0100

    Mock endpoints

--- index.ts
+++ index.ts
@@ -1,17 +1,33 @@
-// @deno-types="npm:@types/express@4"
-import express from "npm:express@4.18.2";
-
-import { load } from "https://deno.land/std@0.224.0/dotenv/mod.ts";
-
-const env = await load();
+// @deno-types="npm:@types/express"
+import express, { NextFunction, Request, Response } from "npm:express@4.18.2";
+import demoData from "./data_blob.json" assert { type: "json" };
 
 const app = express();
-const port = Number(env["PORT"]) || 3000;
+const port = Number(Deno.env.get("PORT")) || 8000;
 
-app.get("/", (_req, res) => {
-  res.status(200).send("Hello from Deno and Express! 2101zfs");
+const reqLogger = function (req: Request, _res: Response, next: NextFunction) {
+  console.info(`${req.method} request to "${req.url}" by ${req.hostname}`);
+  next();
+};
+
+app.use(reqLogger);
+
+app.get("/users", (_req: any, res: any) => {
+  res.status(200).json(demoData.users);
+});
+
+app.get("/users/:id", (req: any, res: any) => {
+  const idx = Number(req.params.id);
+  for (const user of demoData.users) {
+    if (user.id === idx) {
+      res.status(200).json(user);
+    }
+  }
+  res.status(400).json({ msg: "User not found" });
 });
 
 app.listen(port, () => {
   console.log(`Listening on ${port} ...`);
 });
+
+// https://deno-auth-a-50.deno.dev
